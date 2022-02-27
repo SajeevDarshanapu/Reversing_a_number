d864396eeb9b123e4b5804ca9c3935cdf350985a
SajeevDarshanapu <67383521+SajeevDarshanapu@users.noreply.github.com>
2022-02-27 15:16:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Hey
+{
+
+    public class Hi
+    {
+
+        public static void Main(string[] args)
+        {
+
+
+
+
+            int num, rem, sum = 0;
+            Console.WriteLine("Enter your number");
+            num = Convert.ToInt32(Console.ReadLine());
+            while (num != 0)
+            {
+                rem = num % 10;
+                sum = sum * 10 + rem;
+                num = num / 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
